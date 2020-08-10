00c6ce44eb289c687898b147531819156490e5b4
Crowdbotics <team@crowdbotics.com>
2020-08-10 05:41:53 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -7,7 +7,7 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title11 = models.CharField(max_length=150, blank=True,)
+    titleChanged = models.CharField(max_length=150, blank=True,)
 
     def __str__(self):
         return self.title
@@ -22,7 +22,7 @@ class CustomText(models.Model):
 
 
 class HomePage(models.Model):
-    body = models.TextField()
+    body123 = models.TextField(blank=True,)
 
     @property
     def api(self):
